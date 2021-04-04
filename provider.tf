9bd4a11709bcb489644d900d34b508acd9b32d94
jon@dugong.io <jon@dugong.io>
2021-04-04 18:18:18 +0100

    add helm charts to deploy prometheus stack

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
     region = var.AWS_REGION
 }
+
+provider "helm" {
+  kubernetes {
+  host                   = module.eks.cluster_endpoint
+  config_path = "./kubeconfig_dugong-cluster"
+  }
+  version = "~> 2.1.0"
+}
