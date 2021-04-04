fb7de1f550bdcabe96920e69779801fde6cc253e
jon@dugong.io <jon@dugong.io>
2021-04-04 18:20:50 +0100

    typo

--- vars.tf
+++ vars.tf
@@ -1,4 +1,4 @@
-# Set the region that you want to use to host the factorio server and any other AWS-hosted infrastructure
+# Set the region that you want to use to host the AWS-hosted infrastructure
 variable "AWS_REGION" {    
     default = "eu-west-2"
 }
