fcaee7e700f3d564d454b24af3ff99f37780eaa5
jon@dugong.io <jon@dugong.io>
2021-04-05 16:30:07 +0100

    initial add of alb-ingress-controller, although it doesnt yet work

--- terraform/infra/service/helm.tf
+++ terraform/infra/service/helm.tf
@@ -7,6 +7,28 @@ data "aws_eks_cluster_auth" "dugong-cluster-auth" {
 	name = "dugong-cluster"
 }
 
+data "aws_s3_bucket_object" "cluster_id" {
+  bucket = "terraform-dugong-s3-outputs"
+  key    = "cluster_id"
+}
+
+locals {
+	clusterName =  data.aws_s3_bucket_object.cluster_id.body
+}
+
+# Deploy the AWS Load Balancer Controller
+resource "helm_release" "aws-load-balancer-controller" {
+	name = "aws-load-balancer-controller"
+	repository = "https://aws.github.io/eks-charts"
+	chart = "aws-load-balancer-controller"
+	namespace = "kube-system"
+  
+	values = [<<EOF
+clusterName: data.aws_s3_bucket_object.cluster_id.body
+	EOF
+	]
+}
+
 # Deploy the kube-prometheus-stack service
 resource "helm_release" "kube-prometheus-stack" {
 	name             = "kube-prometheus-stack"
